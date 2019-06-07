3ffdfd469a74bd00d084b3691d8952396f238bb2
Yusiel Rodríguez Esquerra <yusiel.re@gmail.com>
2019-06-07 01:45:39 -0300

    Me quedé en el video 11

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Shop.Web.Data;
-using Shop.Web.Data.Entities;
+﻿
 
 namespace Shop.Web
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Data;
+    using Data.Entities;
+    using Helpers;
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -47,6 +44,9 @@ namespace Shop.Web
             });
 
             services.AddTransient<SeedDb>();
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IUserHelper, UserHelper>();
+
 
             services.Configure<CookiePolicyOptions>(options =>
             {
