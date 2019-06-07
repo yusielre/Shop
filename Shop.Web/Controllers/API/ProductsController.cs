cb956fc57eff5267a1786edd96ae34957e93732a
Yusiel Rodríguez Esquerra <yusiel.re@gmail.com>
2019-06-07 12:00:10 -0300

    API Products Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
