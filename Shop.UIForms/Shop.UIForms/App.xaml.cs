52ceb44ef038c018678824488223423ce5c507eb
Yusiel Rodríguez Esquerra <yusiel.re@gmail.com>
2019-06-11 03:12:39 -0300

    LoginPage 1.0

--- Shop.UIForms/Shop.UIForms/App.xaml.cs
+++ Shop.UIForms/Shop.UIForms/App.xaml.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace Shop.UIForms
+﻿namespace Shop.UIForms
 {
+    using Shop.UIForms.Views;
+    using Xamarin.Forms;
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
---
+++ Shop.UIForms/Shop.UIForms/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.UIForms
+{
+    using Shop.UIForms.ViewModels;
+
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
