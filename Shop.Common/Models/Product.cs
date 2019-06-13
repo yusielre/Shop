0a3f3a40989b0ec0f76ee3a20a051ea07674bd83
Yusiel Rodríguez Esquerra <yusiel.re@gmail.com>
2019-06-13 16:19:40 -0300

    Hasta el ApiService

---
+++ Shop.Common/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace Shop.Common.Models
+{
+    using Newtonsoft.Json;
+
+    public class Product
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("price")]
+        public long Price { get; set; }
+
+        [JsonProperty("imageUrl")]
+        public object ImageUrl { get; set; }
+
+        [JsonProperty("lastPurchase")]
+        public object LastPurchase { get; set; }
+
+        [JsonProperty("lastSale")]
+        public object LastSale { get; set; }
+
+        [JsonProperty("isAvailabe")]
+        public bool IsAvailabe { get; set; }
+
+        [JsonProperty("stock")]
+        public long Stock { get; set; }
+
+        [JsonProperty("user")]
+        public User User { get; set; }
+
+        [JsonProperty("imageFullPath")]
+        public object ImageFullPath { get; set; }
+    }
+}
--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -54,7 +54,7 @@
         // POST: Products/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(ProductViewModel view)
+        public async Task<IActionResult>  Create(ProductViewModel view)
         {
             if (ModelState.IsValid)
             {
@@ -62,17 +62,20 @@
 
                 if (view.ImageFile != null && view.ImageFile.Length > 0)
                 {
+                    var guid = Guid.NewGuid().ToString();
+                    var file = $"{guid}.jpg";
+
                     path = Path.Combine(
                         Directory.GetCurrentDirectory(),
                         "wwwroot\\images\\Products",
-                        view.ImageFile.FileName);
+                        file);
 
                     using (var stream = new FileStream(path, FileMode.Create))
                     {
                         await view.ImageFile.CopyToAsync(stream);
                     }
 
-                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                    path = $"~/images/Products/{file}";
                 }
                 var product = this.ToProduct(view, path);
                 // TODO: Pending to change to: this.User.Identity.Name
@@ -149,17 +152,20 @@
 
                     if (view.ImageFile != null && view.ImageFile.Length > 0)
                     {
+                        var guid = Guid.NewGuid().ToString();
+                        var file = $"{guid}.jpg";
+
                         path = Path.Combine(
                             Directory.GetCurrentDirectory(),
                             "wwwroot\\images\\Products",
-                            view.ImageFile.FileName);
+                            file);
 
                         using (var stream = new FileStream(path, FileMode.Create))
                         {
                             await view.ImageFile.CopyToAsync(stream);
                         }
 
-                        path = $"~/images/Products/{view.ImageFile.FileName}";
+                        path = $"~/images/Products/{file}";
                     }
                     var product = this.ToProduct(view, path);
 
