d3356d7dddad46d1f259fa08e92543d5a8ec5aa0
Yusiel Rodríguez Esquerra <yusiel.re@gmail.com>
2019-06-23 02:01:41 -0300

    Hasta que me acosté

--- Shop.Web/Controllers/OrdersController.cs
+++ Shop.Web/Controllers/OrdersController.cs
@@ -9,10 +9,14 @@
     public class OrdersController : Controller
     {
         private readonly IOrderRepository orderRepository;
+        private readonly IProductRepository productRepository;
 
-        public OrdersController(IOrderRepository orderRepository)
+        public OrdersController(
+            IOrderRepository orderRepository,
+            IProductRepository productRepository)
         {
             this.orderRepository = orderRepository;
+            this.productRepository = productRepository;
         }
 
         public async Task<IActionResult> Index()
@@ -20,6 +24,13 @@
             var model = await orderRepository.GetOrdersAsync(this.User.Identity.Name);
             return View(model);
         }
+
+        public async Task<IActionResult> Create()
+        {
+            var model = await this.orderRepository.GetDetailTempsAsync(this.User.Identity.Name);
+            return this.View(model);
+        }
+
     }
 
 }
